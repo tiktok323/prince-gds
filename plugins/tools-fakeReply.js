75a55f555c016d48c50ac9836447937d872bb1af
Mr-Ds <158026058+Mr-GDs@users.noreply.github.com>
2026-04-26 15:52:17 +0500

    Update

--- plugins/tools-fakeReply.js
+++ plugins/tools-fakeReply.js
@@ -2,13 +2,13 @@
 export async function before(m, { conn }) {
 
   const nam = "🟢 Prince✍🏻"
-
+const id_ch = "120363199257221654@newsletter"
  
   global.fwc = {
     contextInfo: {
       isForwarded: true,
       forwardedNewsletterMessageInfo: {
-        newsletterJid: id_canal,
+        newsletterJid: id_ch,
         serverMessageId: 100,
         newsletterName: nam
       }
